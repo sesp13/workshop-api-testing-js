2664a400e7b76883397fde8d7239744da20dbe6f
sesp13 <sespinosarte99@gmail.com>
2021-11-10 02:08:23 -0500

    add eslint to the proyect

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,7 +2,7 @@ const agent = require('superagent');
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
 describe('First Api Tests', () => {
   it('Consume GET Service', async () => {
@@ -16,7 +16,7 @@ describe('First Api Tests', () => {
     const query = {
       name: 'John',
       age: '31',
-      city: 'New York',
+      city: 'New York'
     };
 
     const response = await agent.get('https://httpbin.org/get').query(query);
