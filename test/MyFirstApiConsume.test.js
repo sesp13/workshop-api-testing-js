ed81040d009791a8a05b2c118174814e6ca2ae90
sesp13 <sespinosarte99@gmail.com>
2021-11-10 01:49:13 -0500

    create firsts tests

---
+++ test/MyFirstApiConsume.test.js
@@ -0,0 +1,51 @@
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('First Api Tests', () => {
+  it('Consume GET Service', async () => {
+    const response = await agent.get('https://httpbin.org/ip');
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('origin');
+  });
+
+  it('Consume GET Service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York',
+    };
+
+    const response = await agent.get('https://httpbin.org/get').query(query);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.args).to.eql(query);
+  });
+
+  it('Consume HEAD service', async () => {
+    const response = await agent.head('https://httpbin.org/get');
+    expect(response.status).to.equal(200);
+    expect(response.headers).to.have.property('date');
+  });
+
+  it('Consume PATCH Service', async () => {
+    const response = await agent.patch('https://httpbin.org/patch');
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('origin');
+  });
+
+  it('Consume PUT Service', async () => {
+    const response = await agent.put('https://httpbin.org/put');
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('origin');
+  });
+
+  it('Consume DELETE Service', async () => {
+    const response = await agent.delete('https://httpbin.org/delete');
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('origin');
+  });
+});
